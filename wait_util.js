c8527a68bd2820b28e0c802fd3889487264c30e7
RoyShen12 <34259833+RoyShen12@users.noreply.github.com>
2018-06-27 09:17:24 +0800

    Update wait_util.js

--- wait_util.js
+++ wait_util.js
@@ -42,3 +42,24 @@ sleep_util_for(fxx_util(10000), 200).then(() => console.log('ok'))
   await sleep_util_for(fxx_util(10000), 200)
   console.log('end')
 }())
+// testing for babled async await
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+_asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
+  return regeneratorRuntime.wrap(function _callee$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          console.log('start')
+          _context.next = 3
+          return sleep_util_for(fxx_util(10000), 200)
+
+        case 3:
+          console.log('end')
+
+        case 4:
+        case 'end':
+          return _context.stop()
+      }
+    }
+  }, _callee, this)
+}))()
