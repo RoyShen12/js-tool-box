fb51a927905cd7dc6a6ac05e80996d3207e2392d
RoyShen12 <34259833+RoyShen12@users.noreply.github.com>
2018-07-04 16:27:18 +0800

    add picture magnifier

--- picture.js
+++ picture.js
@@ -4,7 +4,31 @@
  * @author shenwenxiao
  */
 import tween from './tween'
-import config from '@cfg'
+
+/**
+ * 实用函数 计算元素相对屏幕左上角的绝对偏移
+ * - referenced from https://blog.csdn.net/esther_heesch/article/details/51074255
+ * @param {HTMLElement} ele - 待计算的html元素
+ */
+const getOffset = function (ele) {
+  const flag = window.navigator.userAgent.indexOf('MSTE 8') > -1
+  let top = ele.offsetTop
+  let left = ele.offsetLeft
+  while (ele.offsetParent) {
+    ele = ele.offsetParent
+    if (flag) {
+      top += ele.offsetTop
+      left += ele.offsetLeft
+    } else {
+      top += ele.offsetTop + ele.clientTop
+      left += ele.offsetLeft + ele.clientLeft
+    }
+  }
+  return {
+    x: left,
+    y: top
+  }
+}
 
 export default {
   /**
@@ -24,6 +48,7 @@ export default {
       // 判断是否是正确的图片文件
       let flag = true
       let fileA = Array.from(file)
+      // console.log(fileA)
       fileA.forEach(v => {
         flag = flag && !/image\//.test(v.type)
       })
@@ -58,7 +83,7 @@ export default {
             // 在canvas中裁剪、压缩并输出base64编码
             dataURL = canvas.toDataURL('image/jpeg', compressionQuality)
             // 存入param对象，更新页面，在新增/更新时传送至后台
-            if (paramContainer[imageProName] instanceof Array) {
+            if (Array.isArray(paramContainer[imageProName])) {
               paramContainer[imageProName].push(dataURL)
             } else {
               paramContainer[imageProName] = []
@@ -72,19 +97,23 @@ export default {
         for (let f of fileA) {
           oFReader.readAsDataURL(f)
         }
+        inputReference.value = ''
       }
     }
   },
   /**
-   * 图片元素放大，必须有max-height,max-width属性和data-full（默认'false'）属性
+   * 图片元素放大
+   * 必须有max-height,max-width属性和data-full（默认'false'）属性。
+   * 可选的参数可以指出图片是由url的后缀指定尺寸的缩略图，这样在执行本函数时会尝试截去url的后缀以获取到原始大小的图片
    * @param {HTMLElement} imgNode - <img>标签的引用
+   * @param {Boolean} hasCliped - 图片是否是由url的后缀指定尺寸的缩略图
    */
-  previewFullScreen(imgNode) {
+  previewFullScreen(imgNode, hasCliped = false) {
     // 获取保留高度
     const topBarH = config.topBarHeight + 4
     // 注入初始大小
-    imgNode.dataset.oriHeight === undefined ? imgNode.dataset.oriHeight = imgNode.style.maxHeight : void(0)
-    imgNode.dataset.oriWidth === undefined ? imgNode.dataset.oriWidth = imgNode.style.maxWidth : void(0)
+    imgNode.dataset.oriHeight === undefined ? imgNode.dataset.oriHeight = imgNode.style.maxHeight : void (0)
+    imgNode.dataset.oriWidth === undefined ? imgNode.dataset.oriWidth = imgNode.style.maxWidth : void (0)
     if (imgNode.dataset.full === 'false') {
       // 蒙版
       const masking = document.createElement('div')
@@ -93,7 +122,7 @@ export default {
       masking.style.position = 'fixed'
       masking.style.top = topBarH + 'px'
       masking.style.bottom = masking.style.left = masking.style.right = '0px'
-      masking.style.zIndex = '1998'
+      masking.style.zIndex = '2998'
       masking.style.backgroundColor = '#fff'
       masking.style.opacity = '0.5'
       masking.style.margin = '0px'
@@ -112,7 +141,10 @@ export default {
       })
       full_node.id = 'full_screen_img_node'
       full_node.src = imgNode.src
-      full_node.style.zIndex = '1999'
+      if (hasCliped) {
+        full_node.src = full_node.src.replace(/(_)\d+(x)\d+(.)/, '.')
+      }
+      full_node.style.zIndex = '2999'
       full_node.style.position = 'fixed'
       full_node.style.margin = 'auto'
       full_node.style.left = '0px'
@@ -142,5 +174,140 @@ export default {
    */
   deleteImageProc(index, paramContainer, imageProName) {
     paramContainer[imageProName].splice(index, 1)
+  },
+  /**
+   * 获取缩略图地址
+   * @param {String} oriPath 
+   * @param {Number} heigth 
+   * @param {Number} width 
+   */
+  getThumbnailPath(oriPath, heigth, width) {
+    const insertBeforeIndex = function (thisstr, index, string) {
+      const front = thisstr.substring(0, index)
+      const rear = thisstr.substring(index)
+      return front + string + rear
+    }
+   return insertBeforeIndex(oriPath, oriPath.lastIndexOf('.'), '_100x75')
+  },
+  /**
+   * 图片放大镜效果
+   * add on 2018/6/26
+   * @param {HTMLImageElement} imgNode - <img>标签的引用
+   * @param {Object} option - 参数，可以只提供部分属性，如 magnifierOnMouseMove(img, {hasCliped: true})
+   *    - hasCliped: 图片是否是由url的后缀指定尺寸的缩略图 
+   *    - mountNode: 放大镜挂载的目的Node，默认是document.body 
+   *    - magnifierSize: 放大镜尺寸 
+   *    - srcMagChunkSize: 图片放大的源块大小 放大倍率等于 magnifierSize / srcMagChunkSize 
+   *    - magPosition: 放大镜放置位置 
+   *      -- relative-mouse: 相对鼠标偏移 (默认) 
+   *      -- fixed: 相对屏幕左上角偏移（固定位置） 
+   *      -- relative-src: 相对源<img>标签偏移 
+   *    - magPositionX: 放大镜水平偏移量，偏移方式由magPosition决定，负值向左，正值向右，fixed模式时负值将隐藏放大镜
+   *    - magPositionY: 放大镜垂直偏移量，偏移方式由magPosition决定，负值向上，正值向下，fixed模式时负值将隐藏放大镜
+   * @param {Function} callback - 可选的回调函数，每次imgNode的onmousemove触发时都会执行此函数并传入此次鼠标事件的引用
+   */
+  magnifierOnMouseMove(imgNode, option = {
+    hasCliped: false,
+    mountNode: document.body,
+    magnifierSize: 420,
+    srcMagChunkSize: 200,
+    magPosition: 'relative-mouse',
+    magPositionX: -420,
+    magPositionY: 0
+  }, callback = function (){}) {
+
+    if (!imgNode || !(imgNode instanceof HTMLImageElement)) {
+      return
+    }
+    // 如果只提供了部分参数，补全其他参数
+    option = Object.assign({
+      hasCliped: false,
+      mountNode: document.body,
+      magnifierSize: 550,
+      srcMagChunkSize: 200,
+      magPosition: 'relative-mouse',
+      magPositionX: -550,
+      magPositionY: 0
+    }, option)
+    // 如果目标不是html Node，拒绝执行
+    if (!(option.mountNode instanceof Node)) {
+      return
+    }
+    // 如果期望相对源图像定位放大镜，并且源图像不存在x/y属性（IE浏览器），则事先计算出其绝对偏移
+    if (option.magPosition === 'relative-src' && !imgNode.x && !imgNode.y) {
+      const ofs = getOffset(imgNode)
+      imgNode.x = ofs.x
+      imgNode.y = ofs.y
+    }
+    let canvas
+    let ctx
+    // create canvas
+    if (!document.getElementById('__magnifier__pic__')) {
+      canvas = document.createElement('canvas')
+      canvas.id = '__magnifier__pic__'
+      ctx = canvas.getContext('2d')
+      canvas.height = option.magnifierSize
+      canvas.width = option.magnifierSize
+      canvas.style.height = option.magnifierSize + 'px'
+      canvas.style.width = option.magnifierSize + 'px'
+      canvas.style.zIndex = '3998'
+      canvas.style.position = 'fixed'
+      ctx.fillRect(0, 0, option.magnifierSize, option.magnifierSize)
+    } else {
+      canvas = document.getElementById('__magnifier__pic__')
+      ctx = canvas.getContext('2d')
+      ctx.fillRect(0, 0, option.magnifierSize, option.magnifierSize)
+    }
+    // （IE9+）
+    const image = new Image()
+    image.crossOrigin = 'anonymous'
+    if (option.hasCliped) {
+      image.src = imgNode.src.replace(/(_)\d+(x)\d+(.)/, '.')
+    } else {
+      image.src = imgNode.src
+    }
+    // mouse move over imgnode
+    imgNode.onmousemove = evt => {
+      try {
+        callback(evt)
+      } catch (error) {
+      }
+      // create the magnifier
+      if (!document.getElementById('__magnifier__pic__')) {
+        console.log('append magnifier canvas on body')
+        option.mountNode.appendChild(canvas)
+      }
+      // 计算出放大镜的位置
+      switch (option.magPosition) {
+        default:
+        case 'relative-mouse':
+          canvas.style.left = (evt.clientX + option.magPositionX) + 'px'
+          canvas.style.right = ''
+          canvas.style.top = (evt.clientY + option.magPositionY) + 'px'
+          canvas.style.bottom = ''
+          break
+        case 'fixed':
+          canvas.style.left = option.magPositionX + 'px'
+          canvas.style.right = ''
+          canvas.style.top = option.magPositionY + 'px'
+          canvas.style.bottom = ''
+          break
+        case 'relative-src':
+          canvas.style.left = (imgNode.x + option.magPositionX) + 'px'
+          canvas.style.right = ''
+          canvas.style.top = (imgNode.y + option.magPositionY) + 'px'
+          canvas.style.bottom = ''
+          break
+      }
+      // transfer
+      const syy = /*option.hasCliped ? evt.offsetY : */evt.offsetY * (image.naturalHeight / imgNode.offsetHeight)
+      const sxx = /*option.hasCliped ? evt.offsetX : */evt.offsetX * (image.naturalWidth / imgNode.offsetWidth)
+      ctx.drawImage(image, sxx, syy, option.srcMagChunkSize, option.srcMagChunkSize, 0, 0, option.magnifierSize, option.magnifierSize)
+    }
+    // mouse out of imgnode
+    imgNode.onmouseout = evt => {
+      // destory the magnifier
+      option.mountNode.removeChild(document.getElementById('__magnifier__pic__'))
+    }
   }
 }
